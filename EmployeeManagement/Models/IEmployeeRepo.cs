f4763d1c9e410f345e2e48a9e40c08d347708553
KIRIT KUMAR PENDYALA <43256842+kirit3@users.noreply.github.com>
2020-06-05 00:41:04 -0500

    Did Migrations and Created connection with SQL database

--- EmployeeManagement/Models/IEmployeeRepo.cs
+++ EmployeeManagement/Models/IEmployeeRepo.cs
@@ -10,5 +10,9 @@ namespace EmployeeManagement.Models
         Employee GetEmployee(int Id);
         IEnumerable<Employee> GetAllEmployee();
         Employee Add(Employee employee);
+
+        Employee Update(Employee employeeChanges);
+
+        Employee Delete(int id);
     }
 }
---
+++ EmployeeManagement/Models/ModelBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee
+                {
+                    Id = 1,
+                    Name = "Mary",
+                    Department = Dept.IT,
+                    Email = "Mary@gmail.com"
+                },
+                new Employee
+                {
+                    Id = 2,
+                    Name = "K",
+                    Department = Dept.HR,
+                    Email = "K@gmail.com"
+                }
+
+                );
+        }
+
+    }
+}
